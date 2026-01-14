ab08e14ac3a5d86ce58722d5487f43cd600a78c9
kaiharvest <indradwiprabowo27@gmail.com>
2026-01-14 23:47:22 +0700

    commit

---
+++ src/main/java/indradwiprabowo/jpa/BelajarPresistanceApiApplication.java
@@ -0,0 +1,13 @@
+package indradwiprabowo.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarPresistanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarPresistanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/indradwiprabowo/jpa/BelajarPresistanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package indradwiprabowo.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarPresistanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

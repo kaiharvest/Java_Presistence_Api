6cd115352ba64f8e664eb83d04481845ece8a5da
kaiharvest <indradwiprabowo27@gmail.com>
2026-01-16 22:45:13 +0700

    Entity Manager Factory

--- src/main/java/indradwiprabowo/jpa/entity/Customer.java
+++ src/main/java/indradwiprabowo/jpa/entity/Customer.java
@@ -2,8 +2,10 @@ package indradwiprabowo.jpa.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "customers")
 public class Customer {
 
     @Id

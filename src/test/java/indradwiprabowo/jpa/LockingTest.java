53aa533a04d5299005c4f48a1d82aaeba25c90a6
kaiharvest <indradwiprabowo27@gmail.com>
2026-02-15 11:49:49 +0700

    Pessimistic Locking

--- src/test/java/indradwiprabowo/jpa/LockingTest.java
+++ src/test/java/indradwiprabowo/jpa/LockingTest.java
@@ -5,6 +5,7 @@ import indradwiprabowo.jpa.util.JpaUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.LockModeType;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -56,6 +57,42 @@ public class LockingTest {
         entityTransaction.begin();
 
         Brand brand = entityManager.find(Brand.class, "nokia");
+        brand.setName("Nokia Demo 2");
+        brand.setCreatedAt(LocalDateTime.now());
+        brand.setUpdateAd(LocalDateTime.now());
+        entityManager.persist(brand);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void pessimisticLockingDemo1() throws InterruptedException {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Brand brand = entityManager.find(Brand.class, "nokia", LockModeType.PESSIMISTIC_WRITE);
+        brand.setName("Nokia Demo 1");
+        brand.setCreatedAt(LocalDateTime.now());
+        brand.setUpdateAd(LocalDateTime.now());
+
+        Thread.sleep(10 * 1000L);
+        entityManager.persist(brand);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
+    @Test
+    void pessimisticLockingDemo2() throws InterruptedException {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Brand brand = entityManager.find(Brand.class, "nokia", LockModeType.PESSIMISTIC_WRITE);
         brand.setName("Nokia Demo 2");
         brand.setCreatedAt(LocalDateTime.now());
         brand.setUpdateAd(LocalDateTime.now());
